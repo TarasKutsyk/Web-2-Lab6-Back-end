d5fd946310d06f66c124f7cf3cfcb942fbf5da38
xsv7y <taras.kutsyk.pm.2019@lpnu.ua>
2021-11-11 13:49:25 +0200

    Initial commit

---
+++ routes/product.router.js
@@ -0,0 +1,68 @@
+const ProductModel = require('../database/models/Product');
+const router = require('express').Router({strict: true});
+
+router.get('/', async (req, res) => {
+  try {
+    const filter = req.query;
+
+    const products = await ProductModel.find(filter);
+    res.json(products);
+  } catch (e) {
+    console.log('products GET error: ', e.message);
+    res.status(400).send();
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+
+    const product = await ProductModel.findById(id);
+    res.json(product);
+  } catch (e) {
+    console.log('products GET error: ', e.message);
+    res.status(400).send();
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    let newProduct = req.body;
+
+    newProduct = await ProductModel.create(newProduct);
+
+    res.json(newProduct);
+  } catch (e) {
+    console.log('products POST error: ', e.message);
+    res.status(400).send(e.message);
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    const {id, data} = req.body;
+
+    await ProductModel.findByIdAndUpdate(id, data);
+
+    res.send();
+  } catch (e) {
+    console.log('products PUT error: ', e.message);
+    res.status(400).send(e.message);
+  }
+})
+
+router.delete('/', async (req, res) => {
+  const {id} = req.body;
+
+  ProductModel.findByIdAndDelete(id, (err) => {
+    if (err) {
+      console.log('products DELETE error: ', err.message);
+      res.status(400).send(err.message);
+    }
+    else {
+      res.send();
+    }
+  });
+})
+
+module.exports = router;
